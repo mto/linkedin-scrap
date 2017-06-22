8c2cec1c21a4c7108250350a296ada85eafdba8a
Minh Hoang TO <hoang281283@gmail.com>
2017-06-22 17:30:17 +0700

    Remove cookie

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import google as gs
 from pyexcel_xlsx import get_data
 
+import os
+import os.path
+
 
 class Bot(object):
     def __init__(self, xlsx_src, sheet_name='Sheet1', offset=1, search_page_size=5, search_stop=1):
@@ -56,6 +59,25 @@ class Bot(object):
         print('Start collecting company links and processed: ' + str(nb) + " rows")
 
 
+def start_bot():
+    nbr = 0
+    try:
+        nbr = int(open('./.nbread.log').read())
+    except Exception as ex:
+        print(ex)
+
+    nb = 0
+    try:
+        bot = Bot('3000_company_names.xlsx')
+        nb = bot.resume(nbread=nbr) if 0 < nbr <= 3000 else bot.start()
+    finally:
+        fw = open('./.nbread.log', mode='w')
+        fw.write(str(nbr + nb))
+        fw.flush()
+        print('Number of processed companies sofar: ' + str(nbr + nb))
+
+
+'''
 if __name__ == '__main__':
     nbr = 0
     try:
@@ -74,3 +96,16 @@ if __name__ == '__main__':
         fw.write(str(nbr + nb))
         fw.flush()
         print('Number of processed companies sofar: ' + str(nbr + nb))
+'''
+
+if __name__ == '__main__':
+    for i in range(1, 20):
+        try:
+            try:
+                os.remove(os.path.join(os.environ.get('HOME'), '.google-cookie'))
+                print('Remove file .google-cookie')
+            except Exception:
+                pass
+            start_bot()
+        except Exception as ex:
+            print(ex)
